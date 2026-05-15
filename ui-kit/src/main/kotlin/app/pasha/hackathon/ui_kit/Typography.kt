d609181796ce3152e8632e30d4a2cddd9bc0e6c7
huseynliear <elvin.huseynli@kapitalbank.az>
2026-05-15 14:52:16 +0400

    Add typography

--- ui-kit/src/main/kotlin/app/pasha/hackathon/ui_kit/Typography.kt
+++ ui-kit/src/main/kotlin/app/pasha/hackathon/ui_kit/Typography.kt
@@ -1,14 +1,13 @@
-package app.pasha.hackathon.ui.kit
+package app.pasha.hackathon.ui_kit
 
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
-
 object Typography {
 
-    private val Roboto = FontFamily.Default
+    private val Roboto = FontFamily.Companion.Default
 
     private fun textStyle(
         fontWeight: FontWeight,
@@ -23,121 +22,121 @@ object Typography {
     )
 
     val h0: TextStyle = textStyle(
-        fontWeight = FontWeight.Bold,
+        fontWeight = FontWeight.Companion.Bold,
         fontSize = 36,
         lineHeight = 40,
     )
 
     val h1: TextStyle = textStyle(
-        fontWeight = FontWeight.Bold,
+        fontWeight = FontWeight.Companion.Bold,
         fontSize = 32,
         lineHeight = 36,
     )
 
     val h2: TextStyle = textStyle(
-        fontWeight = FontWeight.Bold,
+        fontWeight = FontWeight.Companion.Bold,
         fontSize = 28,
         lineHeight = 32,
     )
 
     val h3: TextStyle = textStyle(
-        fontWeight = FontWeight.Bold,
+        fontWeight = FontWeight.Companion.Bold,
         fontSize = 24,
         lineHeight = 28,
     )
 
     val h4: TextStyle = textStyle(
-        fontWeight = FontWeight.Bold,
+        fontWeight = FontWeight.Companion.Bold,
         fontSize = 20,
         lineHeight = 24,
     )
 
     val s1: TextStyle = textStyle(
-        fontWeight = FontWeight.SemiBold,
+        fontWeight = FontWeight.Companion.SemiBold,
         fontSize = 20,
         lineHeight = 24,
     )
 
     val s2: TextStyle = textStyle(
-        fontWeight = FontWeight.SemiBold,
+        fontWeight = FontWeight.Companion.SemiBold,
         fontSize = 16,
         lineHeight = 20,
     )
 
     val s3: TextStyle = textStyle(
-        fontWeight = FontWeight.SemiBold,
+        fontWeight = FontWeight.Companion.SemiBold,
         fontSize = 14,
         lineHeight = 16,
     )
 
     val l1: TextStyle = textStyle(
-        fontWeight = FontWeight.Normal,
+        fontWeight = FontWeight.Companion.Normal,
         fontSize = 16,
         lineHeight = 20,
     )
 
     val l1m: TextStyle = textStyle(
-        fontWeight = FontWeight.Medium,
+        fontWeight = FontWeight.Companion.Medium,
         fontSize = 16,
         lineHeight = 20,
     )
 
     val l2: TextStyle = textStyle(
-        fontWeight = FontWeight.Normal,
+        fontWeight = FontWeight.Companion.Normal,
         fontSize = 14,
         lineHeight = 16,
     )
 
     val l2m: TextStyle = textStyle(
-        fontWeight = FontWeight.Medium,
+        fontWeight = FontWeight.Companion.Medium,
         fontSize = 14,
         lineHeight = 16,
     )
 
     val b1: TextStyle = textStyle(
-        fontWeight = FontWeight.Normal,
+        fontWeight = FontWeight.Companion.Normal,
         fontSize = 16,
         lineHeight = 24,
     )
 
     val b1m: TextStyle = textStyle(
-        fontWeight = FontWeight.Medium,
+        fontWeight = FontWeight.Companion.Medium,
         fontSize = 16,
         lineHeight = 24,
     )
 
     val b2: TextStyle = textStyle(
-        fontWeight = FontWeight.Normal,
+        fontWeight = FontWeight.Companion.Normal,
         fontSize = 14,
         lineHeight = 20,
     )
 
     val b2m: TextStyle = textStyle(
-        fontWeight = FontWeight.Medium,
+        fontWeight = FontWeight.Companion.Medium,
         fontSize = 14,
         lineHeight = 20,
     )
 
     val c1: TextStyle = textStyle(
-        fontWeight = FontWeight.Normal,
+        fontWeight = FontWeight.Companion.Normal,
         fontSize = 12,
         lineHeight = 16,
     )
 
     val c1sb: TextStyle = textStyle(
-        fontWeight = FontWeight.SemiBold,
+        fontWeight = FontWeight.Companion.SemiBold,
         fontSize = 12,
         lineHeight = 16,
     )
 
     val c2: TextStyle = textStyle(
-        fontWeight = FontWeight.Normal,
+        fontWeight = FontWeight.Companion.Normal,
         fontSize = 10,
         lineHeight = 14,
     )
 
     val c2sb: TextStyle = textStyle(
-        fontWeight = FontWeight.Medium,
+        fontWeight = FontWeight.Companion.Medium,
         fontSize = 10,
         lineHeight = 14,
     )
